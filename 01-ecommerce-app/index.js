bac3af7a75d2e545910f14a3ba1f2341a643f35f
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-04-19 23:40:03 +0700

    init course

---
+++ 01-ecommerce-app/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
